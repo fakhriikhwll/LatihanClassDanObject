c27baf44fa09bb330fca9b6659accb2bcb2606f1
fakhriikhwll <fakhrian.im@gmail.com>
2019-04-02 12:52:36 +0700

    first commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nim : {0}", Nim);
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("Ipk: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "2265";
+            saya.Nama = "Fakhriikhwll";
+            saya.Ipk = 4.0f;
+
+            kamu.Nim = "2269";
+            kamu.Nama = "Ikhwll";
+            kamu.Ipk = 4.0f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
